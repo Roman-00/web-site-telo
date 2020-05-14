3554a418036333ee7016fdd493aaffb2d3c23be3
Roman00 <rvasin00@gmail.com>
2020-05-14 11:20:39 +0600

    feat: configured the modular structure of the project, connected and configured webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Подключение  Полифилов
+
+import "@babel/polyfill";
+import 'formdata-polyfill';
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'es6-promise';
+import 'fetch-polyfill';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable strict */
+const path = require('path');
+
+
+// eslint-disable-next-line no-undef
+module.exports = {
+	entry: {
+		main: './src/index.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		publicPath: '/dist'
+	},
+	devServer: {
+		overlay: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: '/mode_modules/',
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-proposal-object-rest-spread']
+					}
+				}
+			}
+		]
+	}
+};
