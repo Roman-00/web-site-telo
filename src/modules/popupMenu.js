5b86194bd55a58a3e0ba4180fa0204172c4d0ccf
Roman00 <rvasin00@gmail.com>
2020-05-15 15:59:25 +0600

    feat: rewrote using delestrogen

--- src/modules/popupMenu.js
+++ src/modules/popupMenu.js
@@ -6,11 +6,14 @@ const popupMenu = () => {
     btnTopMenu = topMenu.querySelector('img'),
     popupMenu = document.querySelector('.popup-menu');
 
-    btnTopMenu.addEventListener('click', () => {
-      popupMenu.style.display = 'flex';
-    });
-    popupMenu.addEventListener('click', () => {
+    document.body.addEventListener('click', event => {
+      const target = event.target;
+
+      if(target && target.closest('.menu-button')) {
+        popupMenu.style.display = 'flex';
+      } else if(target && (target.tagName === 'A' || !target.classList.contains('close-menu-btn>img'))) {
         popupMenu.style.display = 'none';
+      }
     });
 };
 
